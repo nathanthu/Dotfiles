79ba4e96479a9b871e44729dc8bc6bae935fc97f
bzbzz <nathan.thuault@protonmail.com>
2026-05-21 16:37:15 +0200

    feat: add install script

--- install.sh
+++ install.sh
@@ -1,54 +1,112 @@
-# Creating .config if it does not exist
+#!/bin/bash
+set -e
+
+# Helpers
+
+info() { echo -e "\e[34m[INFO]\e[0m  $*"; }
+ok()   { echo -e "\e[32m[ OK ]\e[0m  $*"; }
+warn() { echo -e "\e[33m[WARN]\e[0m  $*"; }
+die()  { echo -e "\e[31m[FAIL]\e[0m  $*" >&2; exit 1; }
+
+# Sanity check
+
+[[ -d "$HOME/dot" ]] || die "~/dot not found. Rename your cloned repo to 'dot' first."
+
+# Base dirs
+
+info "Creating ~/.config if needed..."
 mkdir -p "$HOME/.config"
+ok "~/.config ready"
 
-mv "$HOME/Dotfiles" "$HOME/dot"
+# DNF packages
 
-# Set prefix variable
-prefix="$HOME/dot"
+info "Installing DNF packages..."
+sudo dnf install -y neovim kitty zsh zoxide bat fzf eza
+ok "DNF packages installed"
 
-dnf install neovim kitty zsh zoxide bat
+# COPR: lazygit
 
-# Symlink Neovim config
-ln -sfn "$prefix/nvim" "$HOME/.config/nvim"
+info "Enabling COPR for lazygit..."
+sudo dnf copr enable -y dejan/lazygit
+info "Installing lazygit..."
+sudo dnf install -y lazygit
+ok "lazygit installed"
 
-# Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# COPR: yazi
 
-# Symlink zsh config
-ln -sfn "$prefix/.zshrc" "$HOME/.zshrc"
+info "Enabling COPR for yazi..."
+sudo dnf copr enable -y lihaohong/yazi
+info "Installing yazi..."
+sudo dnf install -y yazi
+ok "yazi installed"
 
-# zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# Flatpak: Obsidian
 
-# zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+info "Installing Obsidian via Flatpak..."
+flatpak install -y flathub md.obsidian.Obsidian
+ok "Obsidian installed"
 
-# zsh-completions
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
+# Oh My Zsh
 
-# fzf-tab
-git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+if [[ -d "$HOME/.oh-my-zsh" ]]; then
+    warn "Oh My Zsh already installed, skipping"
+else
+    info "Installing Oh My Zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    ok "Oh My Zsh installed"
+fi
 
-# Install Lazygit
-sudo dnf copr enable dejan/lazygit
-sudo dnf install lazygit
+# Powerlevel10k
 
-# Symlink Kitty config
-ln -sfn "$prefix/kitty" "$HOME/.config/kitty"
+P10K_DIR="$HOME/.oh-my-zsh/custom/themes/powerlevel10k"
+if [[ -d "$P10K_DIR" ]]; then
+    warn "Powerlevel10k already installed, skipping"
+else
+    info "Installing Powerlevel10k..."
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$P10K_DIR"
+    ok "Powerlevel10k installed"
+fi
 
+# ZSH plugins
 
-# Symlink tmux config
-ln -sfn "$prefix/tmux/.tmux.conf" "$HOME/.tmux.conf"
+ZSH_PLUGINS_DIR="$HOME/.oh-my-zsh/custom/plugins"
 
-# Symlink obsidian vimrc and snippets
-mkdir -p "$HOME/obs/Vault/99 - Meta"
-mkdir -p "$HOME/obs/Vault/.obsidian"
-ln -sfn "$prefix/obsidian/obsidian.vimrc" "$HOME/obs/Hive/99 - Meta/obsidian.vimrc"
-ln -sfn "$prefix/obsidian/snippets" "$HOME/obs/Hive/.obsidian/snippets"
+clone_plugin() {
+    local name="$1"
+    local url="$2"
+    if [[ -d "$ZSH_PLUGINS_DIR/$name" ]]; then
+        warn "$name already installed, skipping"
+    else
+        info "Installing $name..."
+        git clone --depth=1 "$url" "$ZSH_PLUGINS_DIR/$name"
+        ok "$name installed"
+    fi
+}
 
-# Symlink pandoc filters
-ln -sfn "$prefix/.pandoc" "$HOME/.pandoc"
+clone_plugin "zsh-autosuggestions"     "https://github.com/zsh-users/zsh-autosuggestions"
+clone_plugin "zsh-syntax-highlighting" "https://github.com/zsh-users/zsh-syntax-highlighting"
+clone_plugin "zsh-completions"         "https://github.com/zsh-users/zsh-completions"
+clone_plugin "fzf-tab"                 "https://github.com/Aloxaf/fzf-tab"
 
-# Install pandoc + LaTeX, commented cause full LaTeX is heavy
-# sudo dnf install pandoc
-# sudo dnf install texlive-scheme-full
+# Default shell
+
+info "Changing default shell to zsh..."
+chsh -s "$(which zsh)"
+ok "Default shell set to zsh"
+
+# Pandoc (main pc only, uncomment to install, but never verified yet)
+# info "Installing pandoc..."
+# sudo dnf install -y pandoc
+# ok "pandoc installed"
+
+# Done
+echo ""
+echo -e "\e[32m══════════════════════════════════════════════\e[0m"
+echo -e "\e[32m  Installation complete.\e[0m"
+echo -e "\e[32m══════════════════════════════════════════════\e[0m"
+echo ""
+echo "  Next steps:"
+echo "    1. Run ./symlink.sh to create all config symlinks"
+echo "    2. Log out and back in for zsh to become your default shell"
+echo ""
+warn "Logout required for shell change to take effect."
